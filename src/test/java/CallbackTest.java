288948cada17fbf4e27985b019f339e564b295b5
GalinaBaeva <baeva_galina@mail.com>
2020-06-01 19:44:07 +0300

    tests improved

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -30,9 +30,9 @@ class CallbackTest {
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Василий");
         elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
@@ -42,7 +42,7 @@ class CallbackTest {
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Vasiliy");
         elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
         String text = driver.findElement(By.className("input__sub")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
