e4d548a8dc9be2e72e7a7fed7e612dc19887dd2a
GalinaBaeva <baeva_galina@mail.com>
2020-06-01 15:54:39 +0300

    tests modified

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,45 +1,39 @@
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.*;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import java.util.List;
+        import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class CallbackTest{
+class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
     }
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         driver = new ChromeDriver();
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
-        driver=null;
+        driver = null;
     }
 
     @Test
-    void shouldSubmitRequest(){
-    driver.get("http://localhost:9999");
-    List<WebElement> elements=driver.findElements(By.className("input__control"));
-    elements.get(0).sendKeys("Василий");
-    elements.get(1).sendKeys("+79270000000");
-    driver.findElement(By.className("checkbox__control")).click();
-    driver.findElement(By.className("button")).click();
-    String text=driver.findElement(By.className("alert-success")).getText();
-    assertEquals("Ваша заявка успешно отправлена!",text.trim());
-}
-
-    @Test
-    void shouldTestSomething(){
-        throw new UnsupportedOperationException();
+    void shouldSubmitRequest() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("alert-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена!", text.trim());
     }
+
 }
